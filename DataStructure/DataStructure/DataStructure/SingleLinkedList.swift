f9c599fe2bfca168a6af9a16a5c70b7ff17cda5c
steve_ieunsoo <lesljk@naver.com>
2024-10-17 12:01:54 +0900

    no message

--- DataStructure/DataStructure/DataStructure/SingleLinkedList.swift
+++ DataStructure/DataStructure/DataStructure/SingleLinkedList.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+
+class Node<T> {
+    let value: T?
+    var next: Node?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    init(){
+        self.value = nil
+        self.next = nil
+    }
+}
+
+
+class SingleLinkedList<T> {
+    private var head: Node<T>?
+    
+    func append(_ value: T) {
+        let node = Node(value)
+        if let head = head {
+            head.next = node
+        } else {
+            self.head = node
+        }
+    }
+    func insert(value: T, at: Int){
+        var tmpNode = Node<T>(value)
+        
+        if let head = head {
+            if at == 0 {
+                tmpNode.next = head
+                self.head = tmpNode
+            }else{
+                
+            }
+        }else{
+            self.head = tmpNode
+        }
+        
+    }
+    func removeFirst(){}
+    func removeLast(){}
+    func remove(at: Int){}
+    func find(value: T) -> Node<T>{return Node()}
+    func get(at: Int) -> Node<T>{return Node()}
+    func contains(value: T){}
+    func isEmpty() -> Bool{
+        if let head = head {
+            return false
+        }else{
+            return true
+        }
+    }
+    func size() -> Int{return 0}
+    func reverse(){}
+    func printList(){
+        var cursor: Node<T>? = head
+        
+        //while let을 사용하는 방법 찾아보기
+        
+    }
+    
+}
+
+
---
+++ DataStructure/DataStructure/DesignPattern/Iterator/Iterator.swift
@@ -0,0 +1,105 @@
+//
+//  Iterator.swift
+//  DataStructure
+//
+//  Created by lee eunsoo on 9/14/24.
+//
+
+import Foundation
+
+// 프로토콜 정의
+protocol Iterable {
+    associatedtype E
+    
+    func iterator() -> E
+}
+
+protocol Iterator {
+    associatedtype E
+    
+    func hasNext() -> Bool
+    func next() -> E?
+}
+
+// Book 클래스
+class Book {
+    var name: String = ""
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getName() -> String {
+        return name
+    }
+}
+
+// BookShelf 클래스
+class BookShelf: Iterable {
+    var books: [Book] = []
+    var last: Int = 0
+    
+    init() {
+    }
+    
+    func getBookAt(index: Int) -> Book {
+        return books[index]
+    }
+    
+    func appendBook(book: Book) {
+        books[last] = book
+        last += 1
+    }
+    
+    func getLength() -> Int {
+        return last
+    }
+    
+    // 이터레이터 생성
+    func iterator() -> BookShelfIterator {
+        return BookShelfIterator(bookShelf: self)
+    }
+}
+
+// BookShelfIterator 클래스
+class BookShelfIterator: Iterator {
+    typealias E = Book
+    
+    var bookShelf: BookShelf
+    var index: Int = 0
+    
+    init(bookShelf: BookShelf) {
+        self.bookShelf = bookShelf
+    }
+    
+    func hasNext() -> Bool {
+        return index < bookShelf.getLength()
+    }
+    
+    func next() -> Book? {
+        if hasNext() {
+            let book = bookShelf.getBookAt(index: index)
+            index += 1
+            return book
+        } else {
+            return nil
+        }
+    }
+}
+
+
+// 테스트 코드
+/*
+let bookShelf = BookShelf(maxsize: 5)
+bookShelf.appendBook(book: Book(name: "Swift Programming"))
+bookShelf.appendBook(book: Book(name: "Design Patterns"))
+bookShelf.appendBook(book: Book(name: "Data Structures"))
+
+let it = bookShelf.iterator()
+while it.hasNext() {
+    if let book = it.next() {
+        print(book.getName())
+    }
+}
+*/
+
